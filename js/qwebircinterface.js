4f510a8362bb0c082a660b7de935f16922b308ee
jdcaron <jdcaron@t410lvm.localdomain>
2011-07-16 23:04:27 -0400

    Fix a bug in the feed highlight when the text is colored. 	modified:   qwebircinterface.js

--- js/qwebircinterface.js
+++ js/qwebircinterface.js
@@ -14,7 +14,7 @@ function highlightText(){
     e = document.getElementsByTagName('div');
     for(i = 0; i < e.length; i++){
       if(e[i].getAttribute('class') == 'linestyle1 colourline' || e[i].getAttribute('class') == 'linestyle2 colourline'){
-          if(e[i].childNodes.length == 2){
+          if(e[i].childNodes[1].innerHTML != '=='){
               target = e[i].childNodes[1].childNodes[1].innerHTML;
               e[i].style.fontWeight = '';
               e[i].style.backgroundColor = '';
